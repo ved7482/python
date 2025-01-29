f379ce5799bbaf8062976ebddf2eaee2c78a49e6
Divyanshu. <divyanshunikhade7482@gmail.com>
2025-01-29 08:07:34 +0530

    understand the terms related to input

---
+++ Basics/3.input.py
@@ -0,0 +1,16 @@
+# JAI SHREE RAM
+
+age1 = input("What is age of person1 ? ")           # in general py always takes input as ' string ' datatype so use type casting so as to be more confined 
+print(type(age1))
+
+age2 = int(input("What is age of person2 ? "))
+print(type(age2))
+
+
+# e.g.
+"""
+        What is age of person1 ? 10
+        <class 'str'>
+        What is age of person2 ? 25
+        <class 'int'>
+"""
