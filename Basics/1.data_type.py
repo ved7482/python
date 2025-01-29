94a3b3ef69670994e4c47a3f7998413ffca8615e
Divyanshu. <divyanshunikhade7482@gmail.com>
2025-01-29 08:06:11 +0530

    done with datatypes
    No.s , boolean and string

---
+++ Basics/1.data_type.py
@@ -0,0 +1,44 @@
+# JAI SSHREE RAM
+
+#Data Types
+
+# 1. Numbers
+a = 10                             # but a = "12"  now the type of a is string.
+b, c = 5.6, (12/6)                 #  c = 12/6 = 2.0  gives float value.
+d = 12j
+
+# 2. Boolean
+e, f = False, True                 # e, f = true , false           gives  NameError
+
+# 3. String
+g = 'F'
+h = "Arjun"
+
+
+print(a, type(a)) 
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+print(e, type(e))
+print(f, type(f))
+print(g, type(g))
+print(h, type(h))
+
+
+# print(type(a, b, c, d))           TypeError
+
+
+
+# multi-line comment in python(''' --- ''' or """ ---- """)
+
+# Output
+'''
+        10 <class 'int'>
+        5.6 <class 'float'>
+        2.0 <class 'float'>
+        12j <class 'complex'>
+        False <class 'bool'>
+        True <class 'bool'>
+        F <class 'str'>
+        Arjun <class 'str'>
+'''
