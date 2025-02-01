3f58a903f8f476dbc07ac2dda2e79fe8aa55eab6
Divyanshu. <divyanshunikhade7482@gmail.com>
2025-02-01 19:30:41 +0530

    done

---
+++ Basics/5.greater_no_of_2..py
@@ -0,0 +1,17 @@
+#JAI SHREE RAM
+
+x = int(input('Please enter the 1st no. = '))
+y = int(input('Please enter the 2nd no. = '))
+
+if x >> y:
+    print('The greater no. is %d'%x)
+else:
+    print('The greater no. is %d'%y)
+
+
+'''
+Output => 
+            Please enter the 1st no. = 5
+            Please enter the 2nd no. = 9
+            The greater no. is 9
+'''
