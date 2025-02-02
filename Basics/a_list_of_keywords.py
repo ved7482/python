22c83eb4e730824bfd4c6f37b7857d3b94aee3f7
Divyanshu. <divyanshunikhade7482@gmail.com>
2025-02-02 11:01:31 +0530

    list of keywords

---
+++ Basics/a_list_of_keywords.py
@@ -0,0 +1,55 @@
+import keyword     
+print("\n".join(keyword.kwlist))
+
+# or the write the below code :-
+# for kw in keyword.kwlist:
+#     print(kw)
+
+
+"""
+print(keyword.kwlist)              ==>  prints 35 keywords in the below manner
+
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+"""
+
+
+"""
+OUTPUT ==>
+            False
+            None
+            True
+            and
+            as
+            assert
+            async
+            await
+            break
+            class
+            continue
+            def
+            del
+            elif
+            else
+            except
+            finally
+            for
+            from
+            global
+            if
+            import
+            in
+            is
+            lambda
+            nonlocal
+            not
+            or
+            pass
+            raise
+            return
+            try
+            while
+            with
+            yield
+
+"""
