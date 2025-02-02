26419c164ecf305e505144e4f333889ab4311d51
Divyanshu. <divyanshunikhade7482@gmail.com>
2025-02-02 11:54:43 +0530

    formating of String

---
+++ Basics/6.formating_a_string.py
@@ -0,0 +1,16 @@
+#JAI SHRE RAM 
+
+name = input("Enter your name :- ")
+age = int(input("Enter your age :- "))
+
+# print("My name is",name,"and my age is",age)    
+                
+ #to avoid the use of the , and "" multiple times use "formatted string"
+print(f"My name is {name} and my age is {age}.")
+
+"""
+e.g.
+        Enter your name :- shubhu
+        Enter your age :- 19
+        My name is shubhu and my age is 19.
+"""
