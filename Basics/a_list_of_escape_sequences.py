bfd26f3c88c59c2477af53e25d18d5f227f12499
Divyanshu. <divyanshunikhade7482@gmail.com>
2025-02-02 11:33:34 +0530

    done

---
+++ Basics/a_list_of_escape_sequences.py
@@ -0,0 +1,19 @@
+#JAI SHREE RAM
+
+# raw string {just use r before the string} to print the escape sequences
+escape_sequences = [
+    r"\n - Newline",
+    r"\t - Tab",
+    r"\\ - Backslash",
+    r"\' - Single Quote",
+    r"\" - Double Quote",
+    r"\r - Carriage Return",
+    r"\b - Backspace",
+    r"\f - Form Feed",
+    r"\v - Vertical Tab",
+    r"\a - Bell (alert)",
+    r"\0 - Null character"
+]
+
+for es in escape_sequences:
+    print(es)
