2c6d011b617b86744e561a0329201486fdc0f152
Divyanshu. <divyanshunikhade7482@gmail.com>
2025-04-26 22:31:12 +0530

    completed comparision operators

---
+++ Operators/comparsion.py
@@ -0,0 +1,21 @@
+#JAI SHREE RAM
+
+# Comparision Operators ==> {==, >, <, >=, <=, !=} and they wil always give boolean answers.
+
+print(30>5)                                     #True
+print(12 > 12.1)                                #False ==> comparision of int with float.
+print(12 == 12)                                 #True
+print(12 != 12)                                 #False
+
+print(ord("A"))   #65
+
+print(ord("B"))   #66
+
+print("A" > "B")  #False         
+
+#comparion of strings
+
+print("ABC" < "ACD")  #True  ==> 1st char is same, 2nd char is compared and A < C == True
+
+"""Note ==> comparision of strings is done on the basis of ASCII values of the characters.
+          u can compare str wit a str but u can't compare str with int or float or any other data type."""
---
+++ Operators/logical_op.py
@@ -0,0 +1,10 @@
+#JAI SHREE RAM
+
+#Logical Operators ==> {and, or, not} and they will always give boolean answers.
+
+
+print(9>4 and 11>1 and 61<98)                  #(T and T and T) == True
+print(5<43 or 2>9 or 21<17)                    #(T or F or F) == True
+
+print(not 6<8)                                 #False
+print(not 9> 4 and 7<11 and not 73>52)
