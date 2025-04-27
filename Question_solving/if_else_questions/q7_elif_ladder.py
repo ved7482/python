3a5091c4993c04d2588171a86980982b88cec3f8
Divyanshu. <divyanshunikhade7482@gmail.com>
2025-04-27 08:01:28 +0530

    created a elif ladder

---
+++ Question_solving/if_else_questions/q7_elif_ladder.py
@@ -0,0 +1,26 @@
+#JAI SIYARAM
+
+"""
+take the input of temperature in celsius.
+    - Below 0°C → "Freezing Cold ❄️"
+    - 0°C to 10°C → "Very Cold 🥶"
+    - 10°C to 20°C → "Cold 👘"
+    - 20°C to 30°C → "Pleasant ☁️"
+    - 30°C to 40°C → "Hot 🔥"
+    - Above 40°C → "Very Hot 🌄"
+"""
+
+temp = int(input("Enter the temperature in celsius:- "))
+
+if (temp < 0):
+    print("Freezing Cold ❄️")
+elif (temp >= 0 and temp < 10):
+    print("Very Cold 🥶")
+elif (temp >= 10 and temp < 20):
+    print("Cold 👘")
+elif (temp >= 20 and temp < 30):
+    print("Pleasant ☁️")
+elif (temp >= 30 and temp < 40):
+    print("Hot 🔥")
+else:
+    print("Very Hot 🌄")     
